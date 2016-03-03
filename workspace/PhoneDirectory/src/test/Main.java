245e1e3df21058e1afb136cd8a51ed79d48640ad
Khari <kseals281@gmail.com>
2016-03-02 23:06:26 -0500

    Created a class initilization method for the PhoneDirectory class

--- workspace/PhoneDirectory/src/test/Main.java
+++ workspace/PhoneDirectory/src/test/Main.java
@@ -5,10 +5,11 @@ import main.java.*;
 public class Main {
 
 	public static void main(String[] args) {
+		PhoneDirectory myDirectory = new PhoneDirectory();
 		String name_to_find = "Khari Seals";
 		
-		String number = PhoneDirectory.getNumber(name_to_find);
-		System.out.println(number);
+		String number = myDirectory.getNumber(name_to_find);
+		System.out.println("Khari Seals' number: " + number);
 		
 	}
 
