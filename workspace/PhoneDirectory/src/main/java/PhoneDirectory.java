6141a1fcc7b29a33df032b14801bbebd420d4f75
Khari <kseals281@gmail.com>
2016-03-03 18:50:25 -0500

    Improved the number of calls for the properties

--- workspace/PhoneDirectory/src/main/java/PhoneDirectory.java
+++ workspace/PhoneDirectory/src/main/java/PhoneDirectory.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 public class PhoneDirectory {
 	
 	private Properties prop = new Properties();
+	private String path = null;
 	
 	public PhoneDirectory()
 	{
@@ -15,17 +16,18 @@ public class PhoneDirectory {
 		{
 			prop_input = new FileInputStream("src/main/resources/phone.properties");
 			prop.load(prop_input);
-		} catch (IOException ex) {
+			path = prop.getProperty("path");
+		} catch (IOException err) {
 			// TODO Auto-generated catch block
-			ex.printStackTrace();
+			err.printStackTrace();
 		} finally {
 			if (prop_input != null) {
 				try
 				{
 					prop_input.close();
-				} catch (IOException e)
+				} catch (IOException err)
 				{
-					e.printStackTrace();
+					err.printStackTrace();
 				}
 			}
 		}
@@ -33,19 +35,50 @@ public class PhoneDirectory {
 	
 	public void addEntry(String name, String number)
 	{
+		System.out.println(name + " " + number);
+		try
+		{
+			Writer writer = new FileWriter(path, true);
+			writer.write((name + " " + number) + System.lineSeparator());
+			writer.close();
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
 		
 	}
 	
-	public void deleteEntry(String name)
+	public void deleteEntry(String name) throws IOException
 	{
+		File temp = new File("src/main/resources/temp.txt");
+		File file = new File(path);
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(temp));
 		
+		String line;
+		
+		while( (line = reader.readLine()) != null )
+		{
+			String[] lineArr = new String[3];
+			lineArr = line.split(" ");
+			String catname = (lineArr[0] + " " + lineArr[1]);
+			if (!name.contains(catname))
+			{
+				writer.write(line + System.getProperty("line.separator"));
+			}
+		}
+
+		reader.close();
+		writer.close();
+		file.delete();
+		temp.renameTo(file);
+
 	}
 	
 	public String getNumber(String name) {
 		String number = "";
 		String line;
 		
-		try ( BufferedReader directory = new BufferedReader(new FileReader(prop.getProperty("path"))))
+		try ( BufferedReader directory = new BufferedReader(new FileReader(path)))
 		{			
 			while ((line = directory.readLine()) != null) {
 				String[] lineArr = new String[3]; 
@@ -62,12 +95,18 @@ public class PhoneDirectory {
 			ex.printStackTrace();
 		}
 		return number;
-		
 	}
 	
 	public void changeEntry(String name, String number)
 	{
+		try {
+			deleteEntry(name);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		addEntry(name, number);
 		
 	}
-
+	
 }
